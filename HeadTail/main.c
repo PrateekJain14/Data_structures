4f6a89e1199bb8d99e288a3673fd0ef773f4f7e0
Prateek Jain <prateekjain14feb@gmail.com>
2020-01-01 12:57:36 +0530

    static and global variable concept

--- HeadTail/main.c
+++ HeadTail/main.c
@@ -10,9 +10,20 @@ void fun1(int x){
     // fun1(x-1);  TAIL RECURSION
     }
 }
+
+int fun2(int x){
+ static int n = 0; // static variable are created inside code section and that too only once (lime Global variable)
+ if(x > 0){
+   n++;
+   return fun2(x-1) + n;
+ }
+ return 0 ;
+}
+
 int main()
 {
     int x = 5;
     fun1(x);
+    printf("\nfun2 result  : %d", fun2(x));
     return 0;
 }
