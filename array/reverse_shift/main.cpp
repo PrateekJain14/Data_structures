acc6961de885a0e35ba37fc32597fe85012019ea
Prateek Jain <prateekjain14feb@gmail.com>
2020-01-04 12:06:47 +0530

    array reverse shift and rotate

---
+++ array/reverse_shift/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+struct Array{
+    int A[10];
+    int size;
+    int length;
+};
+
+void display(struct Array arr){
+ for(int i=0;i<arr.length;i++){
+        cout<<"\n"<<arr.A[i];
+    }
+}
+
+void reverseArray(struct Array *arr){
+    int *B;
+    int i,j;
+    B = new int[arr->length];
+    for(i=arr->length-1,j=0;i>=0;i--,j++){
+        B[j]=arr->A[i];
+    }
+    for(i=0;i<=arr->length-1;i++){
+        arr->A[i] = B[i];
+    }
+}
+
+void swaping(int *a,int *b){
+int temp ;
+temp = *a;
+*a= *b;
+*b = temp;
+}
+
+void reverseArraySecond(struct Array *arr){
+    int i,j;
+    for(i=0,j=arr->length-1;i<j;i++,j--){
+       swaping(&arr->A[i],&arr->A[j]);
+    }
+}
+
+void leftShift(struct Array *arr){
+    int i;
+    for(i=0;i<arr->length;i++){
+    arr->A[i] = arr->A[i+1];
+    }
+}
+
+void leftRotation(struct Array *arr){
+    int i;
+    int rotateValue = arr->A[0];
+    for(i=0;i<arr->length-1;i++){
+    arr->A[i] = arr->A[i+1];
+    }
+    arr->A[arr->length-1]=rotateValue;
+}
+
+int main()
+{
+    struct Array arr = {{2,3,4,5,6},10,5};
+    reverseArray(&arr);
+    display(arr);
+    cout<<"\n---------------------------------";
+    reverseArraySecond(&arr);
+    display(arr);
+    cout<<"\n---------------------------------";
+    leftShift(&arr);
+    display(arr);
+    cout<<"\n---------------------------------";
+    leftRotation(&arr);
+    display(arr);
+    return 0;
+}
