8eb7609e1b535dfcdf43c4328493140c2c5173a1
Prateek Jain <prateekjain14feb@gmail.com>
2020-01-06 19:49:51 +0530

    permution of a string recursion

--- string/permutationString/main.cpp
+++ string/permutationString/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-
+#include <cstring>
 using namespace std;
-
+// using another array
 void perm(char s[], int k){
 static int a[10]={0};
 static char res[10];
@@ -22,11 +22,31 @@ else{
 }
 
 }
+void swap(){
+}
+
+void permRecursion(char s[], int l, int h){
+
+int i;
+if(l==h){
+    cout<<s<<endl;
+}
+else{
+    for(i=l;i<=h;i++){
+        swap(s[l],s[i]);
+        permRecursion(s,l+1,h);
+        swap(s[l],s[i]);
+    }
+}
+
+}
+
 
 
 int main()
 {
     char s[]="ABC";
     perm(s,0);
+    permRecursion(s,0,strlen(s)-1);
     return 0;
 }
