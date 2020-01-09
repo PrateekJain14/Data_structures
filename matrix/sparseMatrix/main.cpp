4fb10300b1dc78198a12555424a1d5d7efa100f4
Prateek Jain <prateekjain14feb@gmail.com>
2020-01-09 19:39:41 +0530

    Sparse matrix add operator addee

--- matrix/sparseMatrix/main.cpp
+++ matrix/sparseMatrix/main.cpp
@@ -55,7 +55,42 @@ public:
        return os;
     }
 
+SparseMatrix SparseMatrix::operator+(SparseMatrix &s)
+{
+ int i,j,k;
+ if(row!=s.row || col!=s.col)
+    return SparseMatrix(0,0,0);
 
+ SparseMatrix *sum=new SparseMatrix(row,col,num+s.num);
+
+ i=j=k=0;
+
+ while(i<num && j<s.num)
+ {
+     if(ele[i].i<s.ele[j].i)
+        sum->ele[k++]=ele[i++];
+     else if(ele[i].i > s.ele[j].i)
+        sum->ele[k++]=s.ele[j++];
+     else
+     {
+         if(ele[i].j<s.ele[j].j)
+            sum->ele[k++]=ele[i++];
+         else if(ele[i].j > s.ele[j].j)
+            sum->ele[k++]=s.ele[j++];
+         else
+         {
+            sum->ele[k]=ele[i];
+            sum->ele[k++].x=ele[i++].x+s.ele[j++].x;
+         }
+     }
+ }
+ for(;i<num;i++)sum->ele[k++]=ele[i];
+ for(;j<s.num;j++)sum->ele[k++]=s.ele[j];
+ sum->num=k;
+
+ return *sum;
+
+}
 
 int main()
 {
