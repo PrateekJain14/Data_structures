5159ad2b9e43e8b916413ae2ad18c55d6e6c986c
Prateek Jain <prateekjain14feb@gmail.com>
2020-01-22 19:44:08 +0530

    insert circular doubly LL

--- Node/doublyCircularLL/main.cpp
+++ Node/doublyCircularLL/main.cpp
@@ -7,6 +7,16 @@ struct Node *prev;
 struct Node *next;
 }*first=NULL;
 
+int len(struct Node *ptr){
+int len=0;
+do
+{
+    len++;
+    ptr = ptr->next;
+}while(ptr != first);
+return len;
+}
+
 void create(int a[], int len){
 struct Node *t, *last;
 first = new Node;
@@ -26,6 +36,30 @@ last = first;
  first->prev = last;
 }
 
+void insert(struct Node *ptr,int pos, int ele){
+    if(pos<0 || pos>len(ptr)){
+        return;
+    }
+     struct Node *t;
+     t = new Node;
+     t->data = ele;
+    if(pos==0){
+        t->next = first;
+        first->prev->next = t;
+        t->prev = first->prev;
+        first->prev = t;
+        first = t;
+    }
+    else{
+        for(int i=0;i<pos-1;i++)
+            ptr = ptr->next;
+        t->next = ptr->next;
+        t->prev = ptr;
+        ptr->next->prev = t;
+        ptr->next = t;
+    }
+}
+
 void display(struct Node *ptr){
 do
 {
@@ -38,6 +72,9 @@ int main()
 {
     int a[] = {1,2,3,4,5,6,7,8,9,10};
     create(a,10);
+    display(first);
+    cout<<"\n===========\n";
+    insert(first,3,11);
     display(first);
     return 0;
 }
