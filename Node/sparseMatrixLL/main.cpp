09fd1cbd5eaea8465ea2b0dd32d39501ff4ecaae
Prateek Jain <prateekjain14feb@gmail.com>
2020-01-25 10:53:50 +0530

    sparse matrix using node representation

---
+++ Node/sparseMatrixLL/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int col;
+    int data;
+    struct Node * next;
+};
+
+
+
+void display(struct Node* ptr)
+{
+    while(ptr != NULL)
+    {
+        cout<<ptr->data<<endl;
+        ptr = ptr->next;
+    }
+}
+
+
+int main()
+{
+    int row,col,nonz;
+    cout<<"\nEnter number of rows :";
+    cin>>row;
+    cout<<"\nEnter total number of columns :";
+    cin>>col;
+
+    // Sparse matrix create
+    struct Node *A[row], *last,*temp;
+    for(int i =0 ; i < row; i++)
+    {
+        cout<<"\nEnter Number of non Zero elements in "<<i+1<<" row :";
+        cin>>nonz;
+        last = NULL;
+        if(nonz > 0)
+        {
+            A[i] = new Node;
+            cout<<"\n Enter column number :";
+            cin>>A[i]->col;
+            cout<<"\n Enter data  :";
+            cin>>A[i]->data;
+            A[i]->next = NULL;
+            last = A[i];
+            if(nonz > 1)
+            {
+                for(int j=1; j<nonz; j++)
+                {
+                    temp = new Node;
+                    cout<<"\n Enter column number :";
+                    cin>>temp->col;
+                    cout<<"\n Enter data  :";
+                    cin>>temp->data;
+                    last->next = temp;
+                    temp->next = NULL;
+                }
+            }
+        }
+    }
+
+    struct Node *ptr;
+
+    for(int i=0;i<row;i++){
+            ptr = A[i];
+        for(int j=0;j<col;j++){
+            if(j == ptr->col){
+                cout<<ptr->data << " ";
+                ptr = ptr->next? ptr->next:ptr;
+            }
+            else{
+                cout<< "0 ";
+            }
+        }
+        cout<<"\n";
+    }
+
+
+    return 0;
+}
