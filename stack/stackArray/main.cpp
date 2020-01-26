7437b45f9d85f63848643b10f6740d0a87c45e59
Prateek Jain <prateekjain14feb@gmail.com>
2020-01-26 14:47:01 +0530

    stack using array complete

--- stack/stackArray/main.cpp
+++ stack/stackArray/main.cpp
@@ -2,58 +2,81 @@
 
 using namespace std;
 
-struct stack{
-int size;
-int top;
-int *s;
+struct stack
+{
+    int size;
+    int top;
+    int *s;
 };
 
-void push(struct *st,int ele){
-if(st->top == st->size-1){
-    cout<<"\nOverflow";
-    return;
+void push(struct stack *st,int ele)
+{
+    if(st->top == st->size-1)
+    {
+        cout<<"\nOverflow";
+    }
+    else
+    {
+        st->top++;
+        st->s[st->top] = ele;
+    }
+
 }
 
-st->top++;
-st->s[st->top] = ele;
+int pop(struct stack *st)
+{
+    if(st->top == -1)
+    {
+        cout<<"\nUnderflow";
+        return -1;
+    }
+    int ele = st->s[st->top];
+    st->top--;
+    return ele;
 }
 
-int pop(struct *st){
-if(st->top == -1){
-    cout<<"\nUnderflow";
-    return -1;
-}
-int ele = st->s[st->top];
-st->top--;
-return ele;
-}
-
-int peek(struct st,int pos){
-if(st.top-pos+1 < 0){
-    return -1;
-}
-return st.s[st.top-pos+1];
+int peek(struct stack st,int pos)
+{
+    if(st.top-pos+1 < 0)
+    {
+        return -1;
+    }
+    return st.s[st.top-pos+1];
 }
 
-int stackTop(struct st){
-if(st.top == -1){
-    return -1;
-}
-return st.s[st.top];
+int stackTop(struct stack st)
+{
+    if(st.top == -1)
+    {
+        return -1;
+    }
+    return st.s[st.top];
 }
 
-bool isEmpty(stack st){
-    if(st.top == -1){
+bool isEmpty(struct stack st)
+{
+    if(st.top == -1)
+    {
         return true;
     }
     return false;
 }
 
-bool isFull(stack st){
-    if(st.top == st.size-1){
+bool isFull(struct stack st)
+{
+    if(st.top == st.size-1)
+    {
         return true;
     }
     return false;
+}
+
+void disp(struct stack st)
+{
+    for(int i=st.top; i>=0; i--)
+    {
+        cout<<"\n"<<st.s[i];
+    }
 }
 
 int main()
@@ -64,6 +87,16 @@ int main()
     st.s = new int[st.size];
     st.top = -1;
 
-    push(st,2);
+    push(&st,10);
+    push(&st,20);
+    push(&st,30);
+    push(&st,40);
+    push(&st,50);
+    push(&st,60);
+    push(&st,70);
+    disp(st);
+    cout<<"\n---------------\n";
+    cout<<"Peek :"<<peek(st,2)<<endl;
+    cout<<"Pop :"<<pop(&st)<<endl;
     return 0;
 }
