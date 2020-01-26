fce16f9cda50b6a56ef237023bc3b09413774a1b
Prateek Jain <prateekjain14feb@gmail.com>
2020-01-26 14:34:15 +0530

    push and pop in stack

--- stack/stackArray/main.cpp
+++ stack/stackArray/main.cpp
@@ -2,8 +2,41 @@
 
 using namespace std;
 
+struct stack{
+int size;
+int top;
+int *s;
+};
+
+void push(struct *st,int ele){
+if(st->top == st->size-1){
+    cout<<"\nOverflow";
+    return;
+}
+
+st->top++;
+st->s[st->top] = ele;
+}
+
+int pop(struct *st){
+if(st->top == -1){
+    cout<<"\nUnderflow";
+    return -1;
+}
+int ele = st->s[st->top];
+st->top--;
+return ele;
+}
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    struct stack st;
+    cout<<"\nEnter size of stack :";
+    cin>>st.size;
+    st.s = new int[st.size];
+    st.top = -1;
+
+    push(st,2);
     return 0;
 }
