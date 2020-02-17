ca0596fed18e70d2ba4c6e632dcd1e9ff0b8500a
Prateek Jain <prateekjain14feb@gmail.com>
2020-02-17 19:29:42 +0530

    find leader method 2

--- practice/leaderArray/main.cpp
+++ practice/leaderArray/main.cpp
@@ -19,10 +19,23 @@ void findLeader(int a[], int size){
  }
 }
 
+void printLeader(int a[], int size){
+ int max_right = a[size-1];
+ cout<<max_right << " ";
+ for(int i = size -2; i >=0;i--){
+    if(a[i]>max_right){
+        max_right = a[i];
+        cout<<max_right << " ";
+    }
+ }
+}
+
 int main()
 {
     int arr[] = {16, 17, 4, 3, 5, 2};
     int n = sizeof(arr)/sizeof(arr[0]);
     findLeader(arr, n);
+    cout<<"\n--------------------\n";
+    printLeader(arr, n);
     return 0;
 }
