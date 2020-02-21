ed146a1e648f0e71f9ca234a2375edfe88470154
Prateek Jain <prateekjain14feb@gmail.com>
2020-02-21 14:35:11 +0530

    learning list

---
+++ STL/list/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <list>
+#include <iterator>
+
+using namespace std;
+
+void printList(list <int> li){
+ list <int> :: iterator it;
+ for(it = li.begin(); it != li.end(); it++){
+    cout<<*it<<" ";
+ }
+ cout<<"\n";
+}
+
+
+int main()
+{
+    list<int> list1 , list2;
+
+    for(int i=0;i<10;i++){
+        list1.push_back(i*5);
+        list2.push_front(i*7);
+    }
+
+    cout<<"LIST 1 : ";
+    printList(list1);
+    cout<<"LIST 2 : ";
+    printList(list2);
+    return 0;
+}
