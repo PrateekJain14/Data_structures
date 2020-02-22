ca42b159131fafa73ae763f56dcfd2873e73daed
Prateek Jain <prateekjain14feb@gmail.com>
2020-02-22 17:29:45 +0530

    Stack STL done

--- STL/stackSTL/main.cpp
+++ STL/stackSTL/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
+void showStack(stack<int> st){
+    while(!st.empty()){
+        cout<<st.top()<<"\t";
+        st.pop();
+    }
+    cout<<"\n============================================\n";
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    stack<int> st;
+    st.push(10);
+    st.push(30);
+    st.push(20);
+    st.push(5);
+    st.push(15);
+
+    showStack(st);
+
+    cout<<"Stack size : "<<st.size()<<endl;
+    cout<<"Stack top : "<<st.top();
+
+    st.pop();
+    cout<<"\n============================================\n";
+    showStack(st);
+
     return 0;
 }
