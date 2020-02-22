03734ba4b27d65f1c32a977eec3c767eb5ba1475
Prateek Jain <prateekjain14feb@gmail.com>
2020-02-22 12:07:19 +0530

    Queue STL operations

--- STL/queueSTL/main.cpp
+++ STL/queueSTL/main.cpp
@@ -2,9 +2,23 @@
 #include <queue>
 
 using namespace std;
+void showQueue(queue<int> q)
+{
+    while(!q.empty())
+    {
+        cout  << q.front()<< '\t';
+        q.pop();
+    }
+    cout << '\n';
 
+}
 int main()
 {
     queue<int> q;
+    for(int i = 1 ; i <= 10 ; i++)
+    {
+        q.push(i*10);
+    }
+    showQueue(q);
     return 0;
 }
