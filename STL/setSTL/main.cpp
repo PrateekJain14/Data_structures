11c63f55e4158d4e848054b748b00a1a6343546b
Prateek Jain <prateekjain14feb@gmail.com>
2020-02-24 11:24:58 +0530

    set STL

---
+++ STL/setSTL/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <set>
+
+// Set contain unique elements and no element is repeated.
+
+using namespace std;
+
+int main()
+{
+    set<int, greater<int>> st;
+    // by default set will store element in ascending order
+    // but we can override that by passing second argument as above
+    return 0;
+}
