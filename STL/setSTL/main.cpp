38ecb49191cc21a7fb091198333794b0a672e031
Prateek Jain <prateekjain14feb@gmail.com>
2020-02-24 11:28:48 +0530

    learning set

--- STL/setSTL/main.cpp
+++ STL/setSTL/main.cpp
@@ -7,8 +7,24 @@ using namespace std;
 
 int main()
 {
-    set<int, greater<int>> st;
+    set<int, greater<int> > st;
     // by default set will store element in ascending order
     // but we can override that by passing second argument as above
+
+    st.insert(10);
+    st.insert(20);
+    st.insert(30);
+    st.insert(40);
+    st.insert(50);
+    st.insert(60);
+    st.insert(60); // here set will store only one 60
+    st.insert(70);
+
+    set<int, greater<int> > :: iterator it;
+    for(it = st.begin(); it != st.end(); ++it){
+        cout<<*it<<" ";
+    }
+    cout<<"\n=========================\n";
+
     return 0;
 }
