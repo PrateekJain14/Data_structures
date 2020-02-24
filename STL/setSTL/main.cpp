8534ee2b462f61158f05b88183e76614f5478d34
Prateek Jain <prateekjain14feb@gmail.com>
2020-02-24 11:33:23 +0530

    learning set erase

--- STL/setSTL/main.cpp
+++ STL/setSTL/main.cpp
@@ -21,10 +21,25 @@ int main()
     st.insert(70);
 
     set<int, greater<int> > :: iterator it;
+    cout<<"Set 1 : ";
     for(it = st.begin(); it != st.end(); ++it){
         cout<<*it<<" ";
     }
     cout<<"\n=========================\n";
 
+    set<int> st1(st.begin(),st.end());
+    cout<<"Set 2 : ";
+    for(it = st1.begin(); it != st1.end(); ++it){
+        cout<<*it<<" ";
+    }
+    cout<<"\n=========================\n";
+
+    st1.erase(st1.begin(),st1.find(30));  // it will delete all element before 30
+    // to delete single element simply use st.erase(element_name);
+    cout<<"Set 2 : ";
+    for(it = st1.begin(); it != st1.end(); ++it){
+        cout<<*it<<" ";
+    }
+    cout<<"\n=========================\n";
     return 0;
 }
