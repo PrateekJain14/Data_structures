1481a9b9c48373ef474ef7167f98d1c03f5888fe
Prateek Jain <prateekjain14feb@gmail.com>
2020-02-25 18:40:31 +0530

    multiset operations

--- STL/multisetSTL/main.cpp
+++ STL/multisetSTL/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <set>
+
+// Set contain unique elements and no element is repeated.
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    multiset<int, greater<int> > st;
+    // by default Multi set will store element in ascending order
+    // but we can override that by passing second argument as above
+
+    st.insert(10);
+    st.insert(20);
+    st.insert(30);
+    st.insert(40);
+    st.insert(50);
+    st.insert(60);
+    st.insert(60);
+    st.insert(70);
+
+    multiset<int, greater<int> > :: iterator it;
+    cout<<"Set 1 : ";
+    for(it = st.begin(); it != st.end(); ++it){
+        cout<<*it<<" ";
+    }
+    cout<<"\n=========================\n";
+
+
     return 0;
 }
