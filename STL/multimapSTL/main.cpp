b8965cde55f986b8268c7e92064b8c0aa074434f
Prateek Jain <prateekjain14feb@gmail.com>
2020-02-26 21:10:27 +0530

    multimap STL  operation

--- STL/multimapSTL/main.cpp
+++ STL/multimapSTL/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    multimap<int, int> mp1;
+
+    mp1.insert({1,10});
+    mp1.insert({2,30});
+    mp1.insert({3,20});
+    mp1.insert({4,50});
+    mp1.insert({5,40});
+    mp1.insert({6,70});
+    mp1.insert({7,60});
+    mp1.insert({7,10});
+
+    multimap<int,int> :: iterator it;
+
+    cout<<"MAP 1 :\n";
+    cout<<"\tKey\tValue\n";
+    for(it = mp1.begin();it != mp1.end(); ++it){
+        cout<<"\t"<<it->first<<"\t"<<it->second<<endl;
+    }
+
     return 0;
 }
