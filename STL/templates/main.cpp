f1beee8cf987aefa78829cf28b3eac9489b8082c
Prateek Jain <prateekjain14feb@gmail.com>
2020-03-03 19:09:34 +0530

    template function

--- STL/templates/main.cpp
+++ STL/templates/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
 using namespace std;
+template <class T>
+T maxValue(T a, T b){
+    if(a>b)
+        return a;
+    else
+        return b;
 
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout <<maxValue<int>(3,5)<< endl;
+    cout <<maxValue(3.5,5.9)<< endl;
+    cout <<maxValue<double>(3.5435443,3.5555555)<< endl;
     return 0;
 }
