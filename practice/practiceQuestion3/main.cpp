454f99e385171f9e004ca6a1dd3236094b4d2cc9
Prateek Jain <prateekjain14feb@gmail.com>
2020-03-09 17:45:31 +0530

    practice question

---
+++ practice/practiceQuestion3/main.cpp
@@ -0,0 +1,12 @@
+/*Given a string s which contains lower alphabetic characters, the task is to check if its possible to remove at most one character
+ from this string in such a way that frequency of each distinct character becomes same in the string.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
