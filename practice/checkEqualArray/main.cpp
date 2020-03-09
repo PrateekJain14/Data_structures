dd34f2ae73ce6e51651d0319bfabcca13f31ef29
Prateek Jain <prateekjain14feb@gmail.com>
2020-03-09 16:22:49 +0530

    practice question

--- practice/checkEqualArray/main.cpp
+++ practice/checkEqualArray/main.cpp
@@ -54,5 +54,40 @@ int main() {
 	}
 	}
 
+	// alternative use algorithm library and sort
+
+	//code
+/*	int t;
+	long size=0;
+	cin>>t;
+	while(t--){
+	int flag = 0;
+	cin>>size;
+	int a[size],b[size];
+	for(int i=0;i<size;i++){
+	    cin>>a[i];
+	}
+	sort(a,a+size);
+	for(int j=0;j<size;j++){
+	    cin>>b[j];
+	}
+	sort(b,b+size);
+	for(int j=0;j<size;j++){
+	    if(a[j] != b[j]){
+	        flag = 1;
+	        break;
+	    }
+	}
+	if(flag == 1){
+	    cout<<"0\n";
+	}
+	else{
+	    cout<<"1\n";
+	}
+
+	}
+
+}*/
+
 	return 0;
 }
