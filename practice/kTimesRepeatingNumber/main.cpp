f7c14b2bae41015a4cf06915ae00eb64cf564a1a
Prateek Jain <prateekjain14feb@gmail.com>
2020-03-09 17:17:34 +0530

    practice question k times repeat

--- practice/kTimesRepeatingNumber/main.cpp
+++ practice/kTimesRepeatingNumber/main.cpp
@@ -4,26 +4,26 @@ using namespace std;
 
 int main() {
 	//code
-	int t,size,k;
+	int t,n,k;
 	cin>>t;
-	map<int,int> mp;
 	while(t--){
-	    int flag =0;
-	    cin>>size>>k;
-	    int a[size];
-	    for(int i=0;i<size;i++){
+	    map<int,int> mp;
+	    int flag = 0;
+	    cin>>n>>k;
+	    long a[n];
+	    for(int i=0;i<n;i++){
 	        cin>>a[i];
 	        mp[a[i]]++;
 	    }
-	    for(int i=0;i<size;i++){
-	        if(mp[a[i]] == k){
+	    for(int i=0;i<n;i++){
+	        if(k == mp[a[i]]){
 	            cout<<a[i]<<endl;
 	            flag = 1;
 	            break;
 	        }
 	    }
-	    if(!flag){
-	        cout<<"-1\n";
+	    if(flag == 0){
+	        cout<<-1<<"\n";
 	    }
 
 	}
