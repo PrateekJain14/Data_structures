5a0099ff1fca5c6340e47e5d3d9f5ddb3d115097
Prateek Jain <prateekjain14feb@gmail.com>
2020-03-14 18:04:32 +0530

    practice question 7

--- practice/practiceQues7/main.cpp
+++ practice/practiceQues7/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+/*
+Given a binary tree you need to check if it follows max heap property or not.
+Input:
+The task is to complete the method which takes one argument, root of Binary Tree. The struct Node has a data part which stores the data,
+ pointer to left child and pointer to right child.There are multiple test cases. For each test case, this method will be called individually.
+*/
 
 using namespace std;
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+bool isHeap(Node * tree)
+{
+ if((tree->left) && (tree->right)){
+     if((tree->left->data > tree->data) && (tree->right->data > tree->data))
+     return false;
+     else
+     return (isHeap(tree->left) && isHeap(tree->right));
+ }
+ else if(tree->left){
+     if(tree->left->data > tree->data)
+     return false;
+     else
+     return (isHeap(tree->left));
+ }
+ else if(tree->right){
+     if(tree->right->data > tree->data)
+     return false;
+     else
+     return (isHeap(tree->right));
+ }
+ else{
+     return true;
+ }
+}
 
 int main()
 {
