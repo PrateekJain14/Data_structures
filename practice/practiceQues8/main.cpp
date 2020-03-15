1a00c63f6f0a8def6831b0d9e7542b41015548a1
Prateek Jain <prateekjain14feb@gmail.com>
2020-03-15 11:07:26 +0530

    practice question 8

---
+++ practice/practiceQues8/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+/*
+Given an array A of size N, the task is to check if the given array represents a Binary Max Heap.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case contains two lines.
+The first line of input contains an integer N denoting the size of the array. Then in the next line are N space separated array elements.
+
+Output:
+If array represents a Binary Max Heap, print "1", else print "0" (without quotes).
+*/
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
