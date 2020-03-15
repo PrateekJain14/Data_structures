401c7ec0cb1a114a95308ff35ef0be3b9c9a537e
Prateek Jain <prateekjain14feb@gmail.com>
2020-03-15 11:10:02 +0530

    practice question 8

--- practice/practiceQues8/main.cpp
+++ practice/practiceQues8/main.cpp
@@ -11,8 +11,45 @@ If array represents a Binary Max Heap, print "1", else print "0" (without quotes
 */
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+bool isMaxHeap(int tree[],int n, int i){
+    int left = 2*i+1;
+    int right = 2*i+2;
+    if(i<n){
+        if(left < n && right < n){
+            if(tree[i]>tree[left] && tree[i]>tree[right])
+            return (isMaxHeap(tree,n,left) && isMaxHeap(tree,n,right));
+            else
+            return false;
+        }
+        else if(left < n){
+             if(tree[i]>tree[left])
+              return (isMaxHeap(tree,n,left));
+             else
+              return false;
+        }
+        else if(right < n){
+             if(tree[i]>tree[right])
+              return (isMaxHeap(tree,n,right));
+             else
+              return false;
+        }
+       else
+          return true;
+    }
+}
+
+int main() {
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        int val = isMaxHeap(a,n,0);
+        cout<<val<<endl;
+    }
+	//code
+	return 0;
 }
