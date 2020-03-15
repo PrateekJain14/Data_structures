919f9aa87701c3f2741d0db8466b869426c8ffe9
Prateek Jain <prateekjain14feb@gmail.com>
2020-03-15 18:21:06 +0530

    practice question 9

--- practice/practiceQues9/main.cpp
+++ practice/practiceQues9/main.cpp
@@ -1,1 +1,12 @@
+/*
+A thief trying to escape from a jail has to cross N walls each with varying heights. He climbs X feet every time. But, due to
+ the slippery nature of those walls, every times he slips back by Y feet.  Now the task is to calculate the total number of jumps
+ required to cross all walls and escape from the jail.
 
+Input:
+The first line of input contains an integer T denoting the no of test cases. Then T test cases follow. Each test case contains
+three space separated integers X, Y, N. Then in the next line are N space separated values denoting the heights ( Ht[] ) of the walls.
+
+Output:
+For each test case in a new line print the total number of jumps.
+*/
