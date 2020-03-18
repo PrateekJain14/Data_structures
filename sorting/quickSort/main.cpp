daafdaf7f86edd5a628215933435ebff278c3239
Prateek Jain <prateekjain14feb@gmail.com>
2020-03-18 19:25:48 +0530

    quick sort update

--- sorting/quickSort/main.cpp
+++ sorting/quickSort/main.cpp
@@ -3,9 +3,22 @@
 using namespace std;
 
 void printArray(int a[], int size){
-cout<<"Sorted Array : ";
-for(int i =0;i < size ;i++)
-    cout<<a[i]<<"\t";
+    cout<<"Sorted Array : ";
+    for(int i =0;i < size ;i++)
+        cout<<a[i]<<"\t";
+}
+
+int partition(int a[], int l, int h){
+   int i = l-1;
+   int pivot = a[h];
+   for(int j=l; j<h;j++){
+      if(a[j] < pivot){
+        i++;
+        swap(&a[i],&a[j]);
+      }
+      swap(&a[i+1],&a[h]);
+      return (i+1);
+   }
 }
 
 void quickSort(int a[], int l, int h){
@@ -21,5 +34,6 @@ int main()
     int a[] = {10, 5, 15, 50, 20, 25};
     int n = sizeof(a)/sizeof(a[0]);
     quickSort(a,0,n-1);
+    printArray(a,n);
     return 0;
 }
